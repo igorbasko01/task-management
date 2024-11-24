77dd25fb94d9c7d6a7771fd518353cd4e714ec78
igorbasko01 <igorbasko@gmail.com>
2024-11-24 22:00:54 +0200

    handle loading a task from string

--- task_cli/task.py
+++ task_cli/task.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+import frontmatter
+
 
 @dataclass
 class TaskHistory:
@@ -30,14 +32,24 @@ class TaskPriority:
 
     @classmethod
     def from_numeric_value(cls, value: int):
-        lowest_priority = sorted(cls._value_map.keys())[-1]
+        lowest_priority = sorted(list(cls._value_map.keys()))[-1]
         # If the value is not in the map, set the priority to the lowest priority
         name = cls._value_map.get(value, cls._value_map[lowest_priority])
         return cls(name=name, numeric_value=value)
+    
+    @classmethod
+    def from_name(cls, name: str):
+        for value, priority in cls._value_map.items():
+            if priority == name:
+                return cls(name=priority, numeric_value=value)
+        raise ValueError(f"Invalid priority name: {name}")
 
 
 
 class Task:
+
+    _categories = ["Bug", "Feature", "Documentation", "Maintenance", "UI/UX", "Security"]
+    _boards = ["Backlog", "In Progress", "Done"]
 
     _task_template = """---
 id: TASK-{task_id}
@@ -46,7 +58,7 @@ created: {created}
 priority: {priority}
 category: {category}
 owner: {owner}
-board: {status}
+board: {board}
 ---
             
 ## Description
@@ -56,24 +68,68 @@ board: {status}
 {notes}
             
 ## History
-- {created} - Created
+{created} - Created
 """
 
     def __init__(self, 
                  task_id: int, 
                  title: str, 
                  description: str, 
-                 priority: str, 
+                 priority: int, 
                  category: str, 
-                 owner: str, 
-                 board: str = "Backlog"):
+                 owner: str,
+                 created: datetime, 
+                 board: str = "Backlog",
+                 notes: str = "",
+                 history: TaskHistory = []):
         self.task_id: int = task_id
         self.title: str = title
-        self.created: datetime = datetime.now()
-        self.priority: TaskPriority = priority
-        self.category: str = category
+        self.created: datetime = created or datetime.now()
+        self.priority: TaskPriority = TaskPriority.from_numeric_value(priority)
+        self.category: str = self._validate_category(category)
         self.owner: str = owner
-        self.board: str = board
+        self.board: str = self._validate_board(board)
         self.description: str = description
-        self.notes: str = ""
-        self.history: TaskHistory = []
+        self.notes: str = notes
+        self.history: TaskHistory = history
+
+    @classmethod
+    def from_string(cls, task_str: str):
+        # Read front matter
+        task = frontmatter.loads(task_str)
+        if not task.metadata:
+            raise ValueError("Invalid task front matter")
+        # extract description and notes
+        description = task.content.split("## Description")[1].split("## Notes")[0].strip()
+        notes = task.content.split("## Notes")[1].split("## History")[0].strip()
+        # extract history
+        history = task.content.split("## History")[1].strip()
+        history = [TaskHistory.from_string(entry) for entry in history.split("\n")]
+        # extract task id
+        task_id = int(task.metadata["id"].split("-")[1])
+        # extract priority
+        priority = TaskPriority.from_name(task.metadata["priority"])
+        created = task.metadata["created"]
+        # Create Task object
+        return cls(
+            task_id=task_id,
+            title=task.metadata["title"],
+            description=description,
+            priority=priority.numeric_value,
+            category=task.metadata["category"],
+            created=created,
+            owner=task.metadata["owner"],
+            board=task.metadata["board"],
+            notes=notes,
+            history=history
+        )
+
+    def _validate_category(self, category: str) -> str:
+        if category not in self._categories:
+            raise ValueError(f"Invalid category: {category}, allowed categories: {self._categories}")
+        return category
+    
+    def _validate_board(self, board: str) -> str:
+        if board not in self._boards:
+            raise ValueError(f"Invalid board: {board}, allowed boards: {self._boards}")
+        return board
--- tests/task_test.py
+++ tests/task_test.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import unittest
 
-from task_cli.task import TaskHistory, TaskPriority
+from task_cli.task import Task, TaskHistory, TaskPriority
 
 
 class TaskPriorityTests(unittest.TestCase):
@@ -33,4 +33,88 @@ class TaskHistoryTests(unittest.TestCase):
         self.assertEqual(history.action, "Created")
 
 class TaskTests(unittest.TestCase):
-    pass    
+    _valid_task_str = """---
+id: TASK-1
+title: Test Task
+created: 2021-01-01 12:00:00
+priority: High
+category: Bug
+owner: Test User
+board: Backlog
+---
+
+## Description
+This is a test task
+
+## Notes
+This is a note
+
+## History
+2021-01-01 12:00:00 - Created
+2021-01-01 12:00:01 - Moved to In Progress
+"""
+
+    _invalid_frontmatter_task_str = """---
+id: TASK-1
+title: Test Task
+created: 2021-01-01 12:00:00
+priority: High
+category: Bug
+owner: Test User
+board: Backlog
+# missing closing frontmatter
+
+
+## Description
+This is a test task
+
+## Notes
+This is a note
+
+## History
+2021-01-01 12:00:00 - Created
+"""
+
+    _invalid_history_task_str = """---
+id: TASK-1
+title: Test Task
+created: 2021-01-01 12:00:00
+priority: High
+category: Bug
+owner: Test User
+board: Backlog
+---
+
+## Description
+This is a test task
+
+## Notes
+This is a note
+
+## History
+2021-01-01 12:00:00 - Created
+12345 - Moved to In Progress
+"""
+    
+    def test_task_from_string_valid(self):
+        task = Task.from_string(self._valid_task_str)
+        self.assertEqual(task.task_id, 1)
+        self.assertEqual(task.title, "Test Task")
+        self.assertEqual(task.created, datetime(2021, 1, 1, 12, 0, 0))
+        self.assertEqual(task.priority.name, "High")
+        self.assertEqual(task.category, "Bug")
+        self.assertEqual(task.owner, "Test User")
+        self.assertEqual(task.board, "Backlog")
+        self.assertEqual(task.description, "This is a test task")
+        self.assertEqual(task.notes, "This is a note")
+        self.assertEqual(len(task.history), 2)
+        self.assertEqual(task.history[0].to_string(), "2021-01-01 12:00:00 - Created")
+        self.assertEqual(task.history[1].to_string(), "2021-01-01 12:00:01 - Moved to In Progress")
+
+    def test_task_from_string_invalid_frontmatter(self):
+        with self.assertRaises(ValueError):
+            Task.from_string(self._invalid_frontmatter_task_str)
+
+    def test_task_from_string_invalid_history(self):
+        with self.assertRaises(ValueError):
+            Task.from_string(self._invalid_history_task_str)
